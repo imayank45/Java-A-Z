86830407e952a6d1a1fe5937c42a75affdb22c07
Mayank Kathane <93125481+imayank45@users.noreply.github.com>
2024-02-01 12:07:57 +0530

    Add files via upload

---
+++ BasicsOfJava.java
@@ -0,0 +1,27 @@
+public class BasicsOfJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(8+8);
+
+        // variables
+        int age = 10;
+        int totalBatBroke = 20;
+
+        // data types
+        boolean male = true;
+        byte height = 15;
+        short students = 180;
+        float pi = 3.14f;
+        double secondPi = 3.14;
+
+        /*System.out.println(male);
+        System.out.println(height);
+        System.out.println(pi);
+        System.out.println(secondPi);*/
+
+        char myLetter = 'A';
+        System.out.println(myLetter);
+
+
+    }
+}
